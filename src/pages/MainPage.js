2d76c8718f5a64d74c26baec20b0d98c29124522
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-05-12 16:11:11 -0700

    Template from wiki

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function MainPage(props) {
+  return (
+    <div>
+      <div className="container">
+        <div className="row mb-3">
+          Hello world!
+        </div>
+      </div>
+    </div>
+  );
+}
